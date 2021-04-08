a457d631bf2f6580493e3a196f2f028d2edeaabf
deekshithk-github <81957488+deekshithk-github@users.noreply.github.com>
2021-04-08 13:58:22 +0530

    update variables.tf

--- variable.tf
+++ variable.tf
@@ -1,18 +1,39 @@
-variable "server_name" {
-    type = "string"
-}
 variable "resource_group_name" {
-    type = "string"
-}  
-variable "location" {
-    type = "string"
+  type        = string
+  description = "RG name in Azure"
 }
-variable "version_server" {
-    type = "string"
+
+variable "resource_group_location" {
+  type        = string
+  description = "RG location in Azure"
 }
-variable "adlogin" {
- type = "string"
- }
-variable "adpassword" {
-    type = "string"
+
+variable "app_service_plan_name" {
+  type        = string
+  description = "App Service Plan name in Azure"
+}
+
+variable "app_service_name" {
+  type        = string
+  description = "App Service name in Azure"
+}
+
+variable "sql_server_name" {
+  type        = string
+  description = "SQL Server instance name in Azure"
+}
+
+variable "sql_database_name" {
+  type        = string
+  description = "SQL Database name in Azure"
+}
+
+variable "sql_admin_login" {
+  type        = string
+  description = "SQL Server login name in Azure"
+}
+
+variable "sql_admin_password" {
+  type        = string
+  description = "SQL Server password name in Azure"
 }
