79679701eedf0b091e19acc868de8dc8f10b869d
deekshithk-github <81957488+deekshithk-github@users.noreply.github.com>
2021-04-08 13:16:27 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable "server_name" {
+    type = "string"
+}
+variable "resource_group_name" {
+    type = "string"
+}  
+variable "location" {
+    type = "string"
+}
+variable "version_server" {
+    type = "string"
+}
+variable "adlogin" {
+ type = "string"
+ }
+variable "adpassword" {
+    type = "string"
+}
