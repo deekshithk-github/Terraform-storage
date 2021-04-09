a9b7e98d1e5b3593459e9d3da7c2659aef23a4ca
deekshithk-github <81957488+deekshithk-github@users.noreply.github.com>
2021-04-09 18:36:48 +0530

    delete

--- appservice.tf
+++
@@ -1,6 +0,0 @@
-module "change_app_service" {
-source = "./module/variable/appservice"
-app_service_plan_id = "${module.db_server.server_id}"
-location            = "East us"
-resource_group_name = "changeresource"
-}
