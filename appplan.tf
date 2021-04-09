aff5d7b0c98f3fc50dd13c051569f0a1f7fc5577
deekshithk-github <81957488+deekshithk-github@users.noreply.github.com>
2021-04-09 18:36:10 +0530

    delete

--- appplan.tf
+++
@@ -1,8 +0,0 @@
-module "change_app_service_plan" {
-source                  = "./module/variable/Appserviceplan"
-app_service_plan_name   = "change_app_plan"
-location                = "East us"
-resource_group_name     = "changeresource"
-tier                    = "Free"
-size                    = "F1"
-}
